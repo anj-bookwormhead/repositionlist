1b16d6b488815a6dc066fe8d871bd96dd54cb43b
Angelic Sanoy <31028354+anj-bookwormhead@users.noreply.github.com>
2023-01-16 22:26:44 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-var mediaList = document.querySelectorAll('.lw-body-bg li');
-var headLine = document.querySelector('.lw-filter-option-lbl');
-var mainMenu = document.querySelector('.lw-filter-option');
-const addedHTML = document.querySelectorAll('span.added-html.js-filter-author-list');
+var mediaList = document.querySelectorAll(".lw-body-bg li");
+var headLine = document.querySelector(".lw-filter-option-lbl");
+var mainMenu = document.querySelector(".lw-filter-option");
+const addedHTML = document.querySelectorAll(
+  "span.added-html.js-filter-author-list"
+);
 
-
+var addUl = document.createElement("ul");
+addUl.classList.add("added-ul");
+addUl.classList.add("js-filter-author-list");
 
 // Change the categories
-headLine.textContent = 'Sections';
+headLine.textContent = "Sections";
 //insert html inside the .lw-filter-option
-console.log(addedHTML)
+
+var arr = [];
+console.log(addedHTML);
 
 for (i = 0; i < mediaList.length; i++) {
-    //   console.log(mediaList[i].textContent);
-
-    if (mediaList[i].textContent === 'audio') {
-        console.log(mediaList[i]);
-        mediaList[i].style.display = 'none';
-
-
-    } else if (mediaList[i].textContent === 'vidéo') {
-        console.log(mediaList[i])
-        mediaList[i].style.display = 'none'
-
-    } else if (mediaList[i].textContent === 'texte') {
-        console.log(mediaList[i])
-        mediaList[i].style.display = 'none'
-
-    } else {
-
-    }
-
-
+  if (mediaList[i].textContent === "audio") {
+    arr.push(mediaList[i].outerHTML);
+    mediaList[i].style.display = "none";
+  } else if (mediaList[i].textContent === "vidéo") {
+    arr.push(mediaList[i].outerHTML);
+    mediaList[i].style.display = "none";
+  } else if (mediaList[i].textContent === "texte") {
+    arr.push(mediaList[i].outerHTML);
+    mediaList[i].style.display = "none";
+  } else {
+  }
 }
 
+mainMenu.innerHTML +=
+  '<span class="added-html js-filter-author-list"><span class="lw-filter-option-lbl">Format</span></span>';
 
-mainMenu.innerHTML += '<span class="added-html js-filter-author-list"><span class="lw-filter-option-lbl">Format</span></span>'
+arr.forEach((item) => {
+  addUl.innerHTML += `${item}`;
+  mainMenu.appendChild(addUl);
+});
+
